1edecbc70da23cc08498d26445db24be7cdfdd6f
japheth2022 <japhethmuli.mj@.com>
2022-12-20 09:11:13 -0800

    11-binary_tree_size.c

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - calculates the size of the tree
+ * @tree: the pointer to the tree
+ * Return: size of tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+
+{
+int size = 0;
+if (tree)
+{
+size += 1;
+size += binary_tree_size(tree->left);
+size += binary_tree_size(tree->right);
+}
+return (size);
+}
